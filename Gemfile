02a41c44acb3f3ba67788f8f452cd745434929f0
Robert Ferney <capnregex@gmail.com>
2010-11-16 13:54:27 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+
+gem 'sqlite3-ruby', :require => 'sqlite3', :group => :development
+
+group :development, :test do
+  gem 'rspec-rails'
+end
+
+group :test, :test do
+  gem 'rspec'
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'cd135a6dd6943cab87142ded5518ba5ee9ea87d4e3f7716028d903aee65ae072254e0f6d9c91dc6e9efe1f0a125fedbb264ce79535b53e09f22e11425fb19e23'
