74371aa20c681659394f464ee31c4e8023474d2b
Robert Ferney <capnregex@gmail.com>
2010-11-17 17:49:41 -0700

    logo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   validate :name, :present => true, :max_length => 40
-
+  def to_s;name.to_s;end
 end
